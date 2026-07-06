9aec19cdff2c928d45890508fcef6db2fde6c998
IZAEL DE SOUZA COSTA <izael.costa@escola.pr.gov.br>
2026-07-06 14:38:17 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,8 @@
-'use strict';
-
-/*
-  firebase-config.js
-
-  Substitua os valores abaixo pelos dados reais do seu projeto Firebase.
-
-  Caminho no Firebase:
-  Console Firebase > Configurações do projeto > Geral > Seus apps > SDK setup and configuration > Config
-
-  Este arquivo foi separado para facilitar a configuração no GitHub Pages.
-*/
-
 window.FIREBASE_CONFIG = {
-  apiKey: 'COLE_AQUI_SUA_API_KEY',
-  authDomain: 'COLE_AQUI.firebaseapp.com',
-  projectId: 'COLE_AQUI',
-  storageBucket: 'COLE_AQUI.appspot.com',
-  messagingSenderId: 'COLE_AQUI',
-  appId: 'COLE_AQUI'
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "SEU-PROJETO.firebaseapp.com",
+  projectId: "SEU-PROJETO",
+  storageBucket: "SEU-PROJETO.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:xxxxxxxxxxxxxxxx"
 };
