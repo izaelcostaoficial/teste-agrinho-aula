1dea62e97d61b2cd94562347a3cd6d53a2714122
IZAEL DE SOUZA COSTA <izael.costa@escola.pr.gov.br>
2026-07-09 21:07:59 -0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "COLE_SUA_API_KEY_AQUI",
-  authDomain: "SEU-PROJETO.firebaseapp.com",
-  projectId: "SEU-PROJETO",
-  storageBucket: "SEU-PROJETO.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:xxxxxxxxxxxxxxxx"
-};
